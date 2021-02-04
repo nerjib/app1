52dbfbffb6419ea6f210dc72660875f788faff80
nerjib <kabirnajib0@gmail.com>
2021-02-04 14:28:45 +0100

    get user

--- routes/users.js
+++ routes/users.js
@@ -18,19 +18,6 @@ router.post('/', (req, res, next) => {
         res.status(200).json({"ff":"hello"});
   });
   
-  router.get('/user', (req, res, next) => {
 
-  const getAllQ = 'SELECT * from users';
-  try {
-    // const { rows } = qr.query(getAllQ);
-    const { rows } = await db.query(getAllQ,[req.params.phase]);
-    return res.status(201).send(rows);
-  } catch (error) {
-    if (error.routine === '_bt_check_unique') {
-      return res.status(400).send({ message: 'User with that EMAIL already exist' });
-    }
-    return res.status(400).send(`${error} jsh`);
-  }
-  })
 
 module.exports = router;
