47ef2e07775fea411106bff3c98b7ab7442eca6f
nerjib <kabirnajib0@gmail.com>
2021-02-04 14:25:21 +0100

    get user

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const db = require('../db/index');
+
 
 const router = express.Router();
 
@@ -15,6 +17,20 @@ router.post('/', (req, res, next) => {
   router.get('/', (req, res, next) => {
         res.status(200).json({"ff":"hello"});
   });
+  
+  router.get('/user', (req, res, next) => {
 
+  const getAllQ = 'SELECT * from users';
+  try {
+    // const { rows } = qr.query(getAllQ);
+    const { rows } = await db.query(getAllQ,[req.params.phase]);
+    return res.status(201).send(rows);
+  } catch (error) {
+    if (error.routine === '_bt_check_unique') {
+      return res.status(400).send({ message: 'User with that EMAIL already exist' });
+    }
+    return res.status(400).send(`${error} jsh`);
+  }
+  })
 
 module.exports = router;
